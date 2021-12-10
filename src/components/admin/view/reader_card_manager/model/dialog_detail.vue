019518183aed61c50cf12ebcb39d29d6d94c39ca
huyu <1067715004@qq.com>
2021-12-10 09:31:41 +0800

    新增：同步日志详情

---
+++ src/components/admin/view/reader_card_manager/model/dialog_detail.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-dialog title="同步日志详情" :visible.sync="dialogVisible" width="700px" :before-close="dialogBeforeClose">
+    <div class="pre-line">
+        {{detail}}
+    </div>
+    <!-- <div slot="footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </div> -->
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: 'index',
+  components: {},
+  props: {},
+  data() {
+    return {
+      dialogVisible: false,
+      detail:'',
+    };
+  },
+  computed: {},
+  created() {
+
+  },
+  mounted() { },
+  methods: {
+    show(detail) {
+      this.dialogVisible = true;
+      this.detail = detail;
+      console.log(detail);
+    }
+  },
+};
+</script>
+<style scoped lang='less'>
+.pre-line {
+   white-space: pre-line;
+   font-size: 14px;
+}
+</style>
