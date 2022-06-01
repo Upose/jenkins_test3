f712af90db10937231b89607bc125f106e70f1ee
904678748@qq.com <904678748@qq.com>
2022-06-01 18:36:17 +0800

    403

--- src/components/403.vue
+++ src/components/403.vue
@@ -18,42 +18,48 @@
 
 <script>
 export default {
-  name: '404page',
-  beforeRouteEnter(to,from,next){
-    if(from.fullPath.indexOf('admin')>-1){
-      this.home = 'admin';
-    }
-    next();
-  },
+  name: '403page',
   data () {
     return {
-      code:'index',
-      path:'/#/index',
-      home:'web',
+      path:'',
+      home:'admin',
     }
   },
   mounted(){
-    if(this.home == 'web'){
-      this.code = 'index';
-      this.path = '/#/index?page=1';
-    }else{
-      this.code = 'workbench'
-      this.path = '/workbench/#/admin_workbench';
-    }
+    document.title = '403'
+    this.get403();
   },
   methods:{
+    get403(){
+      let is_admin = window.sessionStorage.getItem('backHome');
+      let path = window.sessionStorage.getItem('backUrl');
+      if(is_admin && is_admin!=null && is_admin!=undefined && is_admin!=''){
+        this.home = is_admin;
+      }
+      if(path && path!=null && path!=undefined && path!=''){
+        this.path = path;
+      }
+    },
+    goBack(){
+      if(this.path && this.path!=''){
+        window.location.replace(this.path);
+      }else{
+        this.goHome();
+      }
+    },
+    goHome(){
+      if(this.home == 'web'){
+        this.linkTo('index','/#/index');//到web首页
+      }else{
+        this.linkTo('workbench','/workbench/#/admin_workbench');//到馆员工作台
+      }
+    },
     linkTo(code, url) {
       if (url) {
         let urlInfo = JSON.parse(localStorage.getItem('urlInfo'));
         let info = urlInfo.find(item => item.code == code);
-        window.location.href = info.path + url;
+        window.location.replace(info.path + url);
       }
-    },
-    goBack(){
-      window.history.go(-1);
-    },
-    goHome(){
-      this.linkTo(this.code,this.path);
     },
   },
 }
