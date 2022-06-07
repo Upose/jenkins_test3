ed4458831ddc5c35206149d7f4e22dc2542db2ca
huyu <1067715004@qq.com>
2022-06-07 13:54:24 +0800

    个人图书馆-检索组件修改

--- src/components/web/view/user/library/model/search-box.vue
+++ src/components/web/view/user/library/model/search-box.vue
@@ -2,8 +2,8 @@
  * @Author: huyu
  * @Date: 2022-06-02 18:25:54
  * @LastEditors: huyu
- * @LastEditTime: 2022-06-02 19:20:01
- * @Description: 
+ * @LastEditTime: 2022-06-07 13:50:37
+ * @Description: 个人图书馆--检索组件--从模板组件复制过来
 -->
 <template>
   <div class="unified_retrieval_sys_temp1-warp my-search-box">
@@ -329,7 +329,7 @@ export default {
         let keyword = this.basicInputKeyWord || "";
         if (keyword.length >= 100) keyword = keyword.substring(0, 100);
         // let href = `${this.webBase}#/web_searchingResult?key=${x.data}&keyword=${encodeURIComponent(keyword)}&id=${encodeURIComponent(columnid)}`;
-        let href = `/articlesearch/#/web_searchingResult?key=${x.data}&keyword=${encodeURIComponent(keyword)}&id=${encodeURIComponent(columnid)}`;
+        let href = `/articlesearch/#/web_searchingResult?key=${x.data}&keyword=${encodeURIComponent(keyword)}&id=${encodeURIComponent(columnid)}&c=${this.cu_colum.id}&p=${this.search.symbol}`;
         this.linkTo('articlesearch', href)
         // window.location.href = href;
         // location.reload();
