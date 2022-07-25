41396b934f09ac05c53e31f80d96941349fe3f03
huyu <1067715004@qq.com>
2022-07-25 17:07:31 +0800

    qq绑定

--- src/components/web/view/user/account_set/model/qq_btn.vue
+++ src/components/web/view/user/account_set/model/qq_btn.vue
@@ -2,12 +2,12 @@
  * @Description: 
  * @Author: wanjikun
  * @Date: 2022-07-19 11:57:11
- * @LastEditTime: 2022-07-19 19:38:36
- * @LastEditors: wanjikun
+ * @LastEditTime: 2022-07-25 17:07:05
+ * @LastEditors: huyu
 -->
 <template>
   <div>
-    <span class="use" id="qqLoginBtn" @click="qqloginBtnClick">立即设置</span> 
+    <span class="use" id="qqLoginBtn" @click="qqloginBtnClick">立即设置</span>
   </div>
 </template>
 
@@ -17,8 +17,8 @@ export default {
   components: {},
   data() {
     return {
-      redirectUrl:'',
-      service:''
+      redirectUrl: '',
+      service: ''
     };
   },
   created() {
@@ -46,7 +46,7 @@ export default {
     //     })
   },
   methods: {
-    qqloginBtnClick(){
+    qqloginBtnClick() {
       let orgInfo = JSON.parse(localStorage.getItem('orgInfo')) || {};
       let redirectUrl = `${this.redirectUrl}/cas/bindQQ?orgcode=${orgInfo.orgCode}&service=${this.service}`
       // console.log('url',url);
@@ -54,9 +54,9 @@ export default {
       //   appId:this.appID,//102013783
       //   redirectURI:url
       // });
-      
+
       let url = `https://graph.qq.com/oauth2.0/authorize?client_id=${this.appID}&redirect_uri=${encodeURIComponent(redirectUrl)}&response_type=code&state=6d9ace8093de42b9a23d80e967e0dfb5`
-      console.log('urlhh哈哈',url);
+      console.log('urlhh哈哈', url);
       location.href = url;
       // window.open(url);
     },
@@ -65,9 +65,41 @@ export default {
         this.redirectUrl = res.data.redirectUrl;
         this.service = res.data.service;
         this.appID = res.data.appID;
-        
+        if (this.$route.query.bind && this.$route.query.bind == 'qq') {
+          this.handleByBind();
+        }
       }).catch((err) => {
       });
+    },
+    // 个人中心绑定QQ进入
+    handleByBind() {
+      let url = window.location.href;
+      url = this.changeURLArg(url, 'bind', 'qqback');
+      window.history.pushState({}, 0, url);
+      this.qqloginBtnClick();
+    },
+    /* 
+    * 修改当前url参数值
+    * url 目标url 
+    * arg 需要替换的参数名称 
+    * arg_val 替换后的参数的值 
+    * return url 参数替换后的url 
+    */
+    changeURLArg(url, arg, arg_val) {
+      var pattern = arg + '=([^&]*)';
+      var replaceText = arg + '=' + arg_val;
+      if (url.match(pattern)) {
+        var tmp = '/(' + arg + '=)([^&]*)/gi';
+        tmp = url.replace(eval(tmp), replaceText);
+        return tmp;
+      } else {
+        if (url.match('[\?]')) {
+          return url + '&' + replaceText;
+        } else {
+          return url + '?' + replaceText;
+        }
+      }
+      return url + '\n' + arg + '\n' + arg_val;
     },
   },
   beforeDestroy() {
@@ -77,7 +109,7 @@ export default {
 </script>
 
 <style scoped lang="less">
-.use{
+.use {
   color: #458dda;
   cursor: pointer;
 }
