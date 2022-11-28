886a3d102ce4bd29c101be1e7a3c92accdf6f36f
huyu <1067715004@qq.com>
2022-11-28 11:35:25 +0800

    安全设置

--- src/components/admin/view/user_set/safe-set.vue
+++ src/components/admin/view/user_set/safe-set.vue
@@ -2,15 +2,15 @@
  * @Author: huyu
  * @Date: 2022-06-24 16:49:05
  * @LastEditors: huyu
- * @LastEditTime: 2022-11-26 19:19:57
+ * @LastEditTime: 2022-11-28 11:33:58
  * @Description: 安全设置
 -->
 <template>
   <div class="content">
     <div class="form-box">
-      <el-form ref="postForm" :model="postForm" label-width="140px" class="admin-form">
+      <el-form ref="postForm" :model="postForm" label-width="140px" class="admin-form" v-loading="loading">
         <el-form-item label="密码安全强度：" v-if="dataKey&&dataKey.passwordStrengthLevel">
-          <el-radio-group v-model="postForm.passwordStrength">
+          <el-radio-group v-model="postForm.passwordStrength" @change="handleChangeDefault">
             <el-radio v-for="(value,key) in dataKey.passwordStrengthLevel" :key="value" :label="value">{{key}}</el-radio>
           </el-radio-group>
         </el-form-item>
@@ -18,6 +18,14 @@
           <el-form-item :label="item.title" v-for="(item,index) in passwordRule[postForm.passwordStrength]" :key="index">
             <el-input v-model="item.value" disabled style="width:90%"></el-input>
             <span>{{item.unit}}</span>
+          </el-form-item>
+          <el-form-item label="登录失败锁定">
+            <el-input v-model.number="postForm.passwordErrorLimit" style="width:90%"></el-input>
+            <span>次/天</span>
+          </el-form-item>
+          <el-form-item label="密码登录保持时长">
+            <el-input v-model.number="postForm.passwordKeepDays" style="width:90%"></el-input>
+            <span>天</span>
           </el-form-item>
         </div>
         <el-form-item label="登录人机验证：">
@@ -41,33 +49,50 @@ export default {
   props: {},
   data() {
     return {
+      loading: false,
       dataKey: null,
       postForm: {
-        passwordStrength: 1,
-        firstLoginChangePassword: false,
-        verifyCodeConfirm: false
+        // passwordStrength: 1,
+        // firstLoginChangePassword: false,
+        // verifyCodeConfirm: false,
+        // passwordErrorLimit: 10,
+        // passwordKeepDays: 30
+      },
+      defaultTime: {
+        '2': {
+          passwordErrorLimit: 5,
+          passwordKeepDays: 15
+        },
+        '1': {
+          passwordErrorLimit: 10,
+          passwordKeepDays: 30
+        },
+        '0': {
+          passwordErrorLimit: 0,
+          passwordKeepDays: 30
+        },
       },
       passwordRule: {
         '2': [
           { title: '密码规则', value: '大写字母+小写字母+数字+特殊字符（. @$!%*#_~?&^）' },
           { title: '密码长度', value: '8-16', unit: '位' },
           { title: '密码过期时间', value: '365', unit: '天' },
-          { title: '登录失败锁定', value: '5', unit: '次/天' },
-          { title: '密码登录保持时长', value: '15', unit: '天' },
+          // { title: '登录失败锁定', value: '5', unit: '次/天', edit: true },
+          // { title: '密码登录保持时长', value: '15', unit: '天', edit: true },
         ],
         '1': [
           { title: '密码规则', value: '大写字母+小写字母+数字' },
           { title: '密码长度', value: '6-16', unit: '位' },
           { title: '密码过期时间', value: '3', unit: '年' },
-          { title: '登录失败锁定', value: '10', unit: '次/天' },
-          { title: '密码登录保持时长', value: '30', unit: '天' },
+          // { title: '登录失败锁定', value: '10', unit: '次/天', edit: true },
+          // { title: '密码登录保持时长', value: '30', unit: '天', edit: true },
         ],
         '0': [
           { title: '密码规则', value: '无' },
           { title: '密码长度', value: '6-16', unit: '位' },
           { title: '密码过期时间', value: '无' },
-          { title: '登录失败锁定', value: '0', unit: '次/天' },
-          { title: '密码登录保持时长', value: '30', unit: '天' },
+          // { title: '登录失败锁定', value: '0', unit: '次/天', edit: true },
+          // { title: '密码登录保持时长', value: '30', unit: '天', edit: true },
         ],
       }
     };
@@ -81,6 +106,7 @@ export default {
   methods: {
     // 获取初始数据
     getKey() {
+      this.loading = true;
       http.getJson('safe-set-init-data').then(res => {
         this.dataKey = res.data;
         this.getData();
@@ -90,7 +116,18 @@ export default {
     getData() {
       http.getJson('security-config').then(res => {
         this.postForm = res.data || this.dataKey.securityConfigData;
+        this.loading = false;
       })
+    },
+    // 切换
+    handleChangeDefault() {
+      // console.log(this.defaultTime[this.postForm.passwordStrength]);
+      // this.postForm.verifyCodeConfirm = 1
+      this.postForm = { ...this.postForm, ...this.defaultTime[this.postForm.passwordStrength] };
+      console.log("🚀 ~ file: safe-set.vue ~ line 124 ~ handleChangeDefault ~ this.postForm", this.postForm)
+      // this.postForm.verifyCodeConfirm = this.defaultTime[this.postForm.passwordStrength].verifyCodeConfirm;
+      // this.postForm.firstLoginChangePassword = this.defaultTime[this.postForm.passwordStrength].firstLoginChangePassword;
+      // this.$set(this.postForm, 'verifyCodeConfirm', this.defaultTime[this.postForm.passwordStrength].verifyCodeConfirm)
     },
     // 保存设置
     handleSubmit() {
