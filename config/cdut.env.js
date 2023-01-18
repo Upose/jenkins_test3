d4f37535c94db95f2b750cfe1d0c369da281f0ec
huyu <1067715004@qq.com>
2023-01-18 11:32:33 +0800

    删除不用的配置文件、生产环境不要console.log

--- config/cdut.env.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-// 成都理工大学环境
-module.exports = {
-  NODE_ENV: '"cdut"',
-  VUE_APP_FILE_URL: 'http://192.168.250.81:80/cdn/'
-}
--- config/cqu.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// 重庆大学环境
-module.exports = {
-  NODE_ENV: '"cqu"',
-  VUE_APP_FILE_URL: 'http://202.202.12.19:800/cdn/'
-  // VUE_APP_BASE_API:'"http://192.168.21.46:8000/"',//api域名
-  // VUE_APP_IMG_URL:'"http://192.168.21.46:6900/"',//文件站点域名
-}
