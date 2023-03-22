8941ccd98a7b0368e774955f55858b72432134c4
gongqin <gongqin@vipinfo.com.cn>
2023-03-22 11:32:26 +0800

    动态配置二级目录名

--- src/router/index.js
+++ src/router/index.js
@@ -4,64 +4,87 @@ import webRouter from './web_router';
 import adminRouter from './admin_router';
 Vue.use(Router)
 
+const dlibAppRouterPrefix = window.dlibAppRouterPrefix;
+const ruterBase = dlibAppRouterPrefix
+  ? dlibAppRouterPrefix + "/usermanage/"
+  : "/usermanage/";
 
 export default new Router({
-  mode: 'history',
-  base: process.env.NODE_ENV == 'production' ? '/usermanage/' : '',// 打包时需要配置二级目录名
+  mode: "history",
+  base: process.env.NODE_ENV == "production" ? ruterBase : "", // 打包时需要配置二级目录名
   routes: [
     //{ path: '/', redirect: '/admin_userManager' },
     {
-      path: '/web',
-      name: 'index',
-      component: r => require.ensure([], () => r(require('@/components/web/common/index')), 'index'),
-      meta: { title: '首页', keepAlive: true },
+      path: "/web",
+      name: "index",
+      component: r =>
+        require.ensure(
+          [],
+          () => r(require("@/components/web/common/index")),
+          "index"
+        ),
+      meta: { title: "首页", keepAlive: true },
       children: webRouter.router,
       async beforeEnter(to, from, next) {
-        if (localStorage.getItem('token')) {
+        if (localStorage.getItem("token")) {
           let response = await axios({
-            url: '/appcenter/api/baseinfo/getauthinfo?appcode=usermanage',
-            method: 'get'
+            url: "/appcenter/api/baseinfo/getauthinfo?appcode=usermanage",
+            method: "get"
           }).then(x => x.data);
-          if (response.data.canWeb) { next(); return }
-          next({ name: '403' })
+          if (response.data.canWeb) {
+            next();
+            return;
+          }
+          next({ name: "403" });
         } else {
-          next()
+          next();
         }
       }
     },
     {
-      path: '/admin',
-      name: 'index',
-      component: r => require.ensure([], () => r(require('@/components/admin/common/index')), 'index'),
-      meta: { title: '首页', keepAlive: true },
+      path: "/admin",
+      name: "index",
+      component: r =>
+        require.ensure(
+          [],
+          () => r(require("@/components/admin/common/index")),
+          "index"
+        ),
+      meta: { title: "首页", keepAlive: true },
       children: adminRouter.router,
       async beforeEnter(to, from, next) {
-        if (localStorage.getItem('token')) {
+        if (localStorage.getItem("token")) {
           let response = await axios({
-            url: '/appcenter/api/baseinfo/getauthinfo?appcode=usermanage',
-            method: 'get'
+            url: "/appcenter/api/baseinfo/getauthinfo?appcode=usermanage",
+            method: "get"
           }).then(x => x.data);
-          if (response.data.canAdmin) { next(); return }
-          next({ name: '403' })
+          if (response.data.canAdmin) {
+            next();
+            return;
+          }
+          next({ name: "403" });
         } else {
-          next()
+          next();
         }
       }
     },
     {
-      path: '/403',
-      name: '403',
-      component: r => require.ensure([], () => r(require('@/components/403')), 'index'),
+      path: "/403",
+      name: "403",
+      component: r =>
+        require.ensure([], () => r(require("@/components/403")), "index")
     },
     {
-      path: '/404',
-      name: '404',
-      component: r => require.ensure([], () => r(require('@/components/404')), 'index'),
+      path: "/404",
+      name: "404",
+      component: r =>
+        require.ensure([], () => r(require("@/components/404")), "index")
     },
     {
-      path: '/500',
-      name: '500',
-      component: r => require.ensure([], () => r(require('@/components/500')), 'index'),
+      path: "/500",
+      name: "500",
+      component: r =>
+        require.ensure([], () => r(require("@/components/500")), "index")
     },
     // {//重定向中间件
     //   path: '/',
@@ -72,8 +95,8 @@ export default new Router({
     //   }
     // },
     {
-      path: '*',
-      redirect: '/404',
+      path: "*",
+      redirect: "/404"
     }
   ]
-})
+});
